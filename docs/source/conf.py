2ff2f8479c5fce848737d5a640fceed7c80d7735
sametz <sametz@udel.edu>
2020-05-10 14:10:25 -0400

    Bump version and updated CHANGELOG.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Geoffrey M. Sametz'
 # The short X.Y version
 version = '0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.5.0-beta'
+release = '0.5.1-beta'
 
 
 # -- General configuration ---------------------------------------------------
