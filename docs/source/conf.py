943b0654f6145d653a8ece7d71217e3515814ea0
sametz <sametz@udel.edu>
2021-07-20 22:37:52 -0400

    Having trouble building docs: using conda environment, doesn't recognize matplotlib. Created a venv with python 3.7.6 but running jupyter notebook results in 'No module named ipykernel_launcher'. Updated copyright in docs and added env/ to .gitignore.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'nmrsim'
-copyright = '2020, Geoffrey M. Sametz'
+copyright = '2021, Geoffrey M. Sametz'
 author = 'Geoffrey M. Sametz'
 
 # The short X.Y version
