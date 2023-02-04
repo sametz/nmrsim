befcdc72300c45f0f65e4755dc3f9ca6ab3d9db0
sametz <sametz@udel.edu>
2023-02-03 22:19:55 -0500

    Removed source_suffix from conf.py, which causes errors that used to be suppressed but are no longer supressed. Docs build now!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst', '.ipynb']
 
 # The master toctree document.
 master_doc = 'index'
@@ -66,7 +65,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
