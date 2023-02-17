4df139f04f954c68946ad701a0f6ab4c743e8bca
sametz <sametz@udel.edu>
2023-02-16 19:26:31 -0500

    Fixing Sphinx version (it doesn't like "from importlib import metadata" :(  )

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,12 +25,13 @@ project = "nmrsim"
 copyright = "2023, Geoffrey M. Sametz"
 author = "Geoffrey M. Sametz"
 
-PACKAGE_VERSION = metadata.version("nmrsim")
-version = release = PACKAGE_VERSION
+# PACKAGE_VERSION = metadata.version("nmrsim")
+# version = release = PACKAGE_VERSION
+
 # The short X.Y version
-# version = '0.5'
+version = "0.6"
 # The full version, including alpha/beta/rc tags
-# release = '0.5.2-beta'
+release = "0.6.0"
 
 
 # -- General configuration ---------------------------------------------------
