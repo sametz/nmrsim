6ea2dd8a34f0975406c2aafc3b2faf8276a64bec
Geoffrey Sametz <sametz@users.noreply.github.com>
2023-12-08 18:05:16 -0500

    fixed formatting

--- src/nmrsim/qm.py
+++ src/nmrsim/qm.py
@@ -286,7 +286,7 @@ def _transition_matrix_dense(nspins):
             # If m is a power of two, it will look like 100[...]00 in binary
             # m-1 will look like 011[...]11 in binary.
             # Using a binary and, we should then get 0.
-            if m > 0 and m & (m-1) == 0:
+            if m > 0 and m & (m - 1) == 0:
                 T[i, j] = 1
     T += T.T
     return T
