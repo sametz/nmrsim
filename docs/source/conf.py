18cbdb95d876fcc2fbd1b85175e0abc5c6722900
Geoffrey Sametz <sametz@users.noreply.github.com>
2026-08-01 10:40:55 -0400

    updated version/year and CHANGELOG.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 # -- Project information -----------------------------------------------------
 
 project = "nmrsim"
-copyright = "2023, Geoffrey M. Sametz"
+copyright = "2026, Geoffrey M. Sametz"
 author = "Geoffrey M. Sametz"
 
 # PACKAGE_VERSION = metadata.version("nmrsim")
