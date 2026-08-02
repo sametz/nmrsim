ca01df7ae0086d8e1a05f240efca8cab5adb8202
Geoffrey Sametz <sametz@users.noreply.github.com>
2026-08-02 17:41:06 -0400

    fixed nmrsim.__version__. This should now get updated automatically from the version in setup.cfg.

--- src/nmrsim/__init__.py
+++ src/nmrsim/__init__.py
@@ -60,9 +60,10 @@ implementation).
 
 """
 
+from importlib.metadata import version
 from ._classes import Multiplet, SpinSystem, Spectrum  # noqa: F401
 
-__version__ = "0.6.0"
+__version__ = version("nmrsim")
 
 # When Python 3.7 sunsets, consider the following for dynamic versioning:
 # __version__ = __import__('importlib.metadata').metadata.version(__package__)
